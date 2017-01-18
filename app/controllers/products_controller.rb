937ecf83c111c81fa67e24e6ab320b63c19a38dd
millarba <millarba@gmail.com>
2017-01-17 20:15:51 -0600

    added bootstrap

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+
+  def one_product
+    @products = Product.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+product = Product.new(name: "Parmeggiano Reggiano", price: 15, color: "pale-ish yellow", description: "VERY fancy cheese")
+product.save
+
+product = Product.new(name: "Romano", price: 6, color: "white", description: "Not the sitcom character")
+product.save
+
+product = Product.new(name: "Whiz", price: 3, color: "neon orange", description: "It comes in a can.  Pretty neat!")
+product.save
+
+product = Product.new(name: "Brie", price: 8, color: "white, with weird rind", description: "Also a woman's name")
+product.save
+
+product = Product.new(name: "White Cheddar", price: 5, color: "See: Name", description: "Like cheddar, but different")
+product.save
