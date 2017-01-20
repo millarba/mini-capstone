c8d18cb5cb16642e4b1ceaa6e923f427aba66d88
millarba <millarba@gmail.com>
2017-01-19 20:21:31 -0600

    added form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,22 @@ class ProductsController < ApplicationController
   def one_product
     @products = Product.all
   end
+
+  def all_products
+    @products = Product.all
+  end
+
+  def add_product
+
+  end
+
+  def product_success
+    @product = Product.new(
+                            name: params["name"],
+                            price: params["price"],
+                            color: params["color"],
+                            description: params["description"]
+      )
+    @product.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '/one_product' => 'products#one_product'
+  get'/product_list' => 'products#all_products'
+
+  get '/add_product_form' => 'products#add_product'
+  post '/add_product_form' => 'products#product_success'
 end
