83761f683a746002dd411885f85acc90198ee51a
millarba <millarba@gmail.com>
2017-01-23 20:29:40 -0600

    added background image

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,8 @@ class ProductsController < ApplicationController
                             description: params[:description]
       )
     @product.save
+    flash[:success] = "Product Created Successfully."
+    redirect_to "/products/#{@product.id}"
   end
 
   def show
@@ -36,10 +38,14 @@ class ProductsController < ApplicationController
     @product.description = params[:description]
 
     @product.save
+    flash[:info] = "Product Updated Successfully."
+    redirect_to "/products/#{@product.id}"
   end
 
   def destroy
     @product = Product.find(params[:id])
     @product.destroy
+    flash[:warning] = "Product Removed Successfully."
+    redirect_to "/products"
   end
 end
