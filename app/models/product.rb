8aad560e9cc0576cdf606ae9a1f89f77a9f0bd8b
millarba <millarba@gmail.com>
2017-01-23 21:29:49 -0600

    end of class

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+
+  def sale_message
+    "Discount Item!" if price < 3 else "Everyday Value!"
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
