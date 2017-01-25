ea0c5f64e318503d25235ea334374e84f9f5ace3
millarba <millarba@gmail.com>
2017-01-24 21:24:10 -0600

    end of class, added stock

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,15 @@
 class Product < ApplicationRecord
 
-  def sale_message
-    "Discount Item!" if price < 3 else "Everyday Value!"
+  # def sale_message
+  #   if price < 3
+  #     "Discount Item!" 
+  #   else 
+  #     "Everyday value!"
+  #   end
+  # end
+
+  def discounted?
+    price < 5
   end
 
   def tax
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118012603) do
+ActiveRecord::Schema.define(version: 20170125023107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.integer  "price"
+    t.decimal  "price",       precision: 5, scale: 2
     t.string   "color"
-    t.string   "description"
+    t.text     "description"
     t.string   "image"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
+    t.boolean  "in_stock",                            default: true
   end
 
 end
