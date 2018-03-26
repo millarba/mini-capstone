f7f22249439d0a7c54c0a67e914debffbffb0fe4
millarba <millarba@gmail.com>
2018-03-26 17:56:00 -0500

    update all gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :orders
   has_many :carted_products
+  has_many :orders
 
   validates :email, presence: true
   validates :email, uniqueness: true
